2f0ef7f4b41bb244e0332aea9579132d7cbd8c1e
Gael Musikingala <gaelmusi0@gmail.com>
2018-06-24 00:57:00 +0200

    front page changes

--- src/App.js
+++ src/App.js
@@ -8,11 +8,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Gael's Profile </h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          You will learn about Gael's career as a developer, the programming languages he masters, etc.
         </p>
+
+          <a href="profile.gaelmusi.com"> Click here</a>
+
       </div>
     );
   }
