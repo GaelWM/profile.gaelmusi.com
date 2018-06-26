478d56489d3c62ff552d4d1209c3c06d4ec119e8
SILICONOVERDRIV\gael <gaelmusi0@gmail.com>
2018-06-26 15:16:10 +0200

    Setting up profile template

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './header.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+	class Profile extends React.Component 
+	{
+    	render() {
+    
+	      return null
+    	}
+  	}
+
+
+	ReactDOM.render(
+    	<Profile />,
+    	document.getElementById('root')
+  	);
 
 
 
