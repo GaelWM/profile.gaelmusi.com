edd6bba83e6a9a5b543878089250a3486bc4ad7b
SILICONOVERDRIV\gael <gaelmusi0@gmail.com>
2018-09-14 17:30:16 +0200

    Add API testing

--- src/RepoCommit.test.js
+++ src/RepoCommit.test.js
@@ -1,31 +1,60 @@
+// __tests__/fetch.js
 import React from "react";
-import Commit from "./RepoCommits";
-import RepoCommits from "./RepoCommits";
-import renderer from "react-test-renderer";
+import {
+  render,
+  fireEvent,
+  cleanup,
+  waitForElement
+} from "react-testing-library";
+// this adds custom jest matchers from jest-dom
+i; //mport "jest-dom/extend-expect";
+import axiosMock from "axios"; // the mock lives in a __mocks__ directory
+import RepoCommits from "./RepoCommits"; // see the tests for a full implementation
 
-test("CommitList renders properly ", () => {
-  const message = "Add login form";
-  const commit = renderer.create(<Commit index={1} message={message} />);
+// automatically unmount and cleanup DOM after the test is finished.
+afterEach(cleanup);
 
-  const RepoCommit = renderer.create(<RepoCommits />);
+test("Fetch makes an API call and displays the commits when Get Last Commits is clicked", async () => {
+  //ini
+  const commits = {
+    "0": { index: "1", message: "Change color of a main div" },
+    "1": { index: "1", message: "Change file transfer process to sftp" }
+  };
+  // Arrange
+  //axiosMock.get.mockResolvedValueOnce({ commits });
+  //const url = "https://api.github.com/repos/gaelwm/profile.gaelmusi.com/commits";
 
-  let tree = RepoCommit.toJSON();
-  expect(tree).toMatchSnapshot();
+  const spy = jest.fn();
+  const { getByTestId, container } = render(<RepoCommits commits={commits} />);
 
-  //console.log(tree);
+  const { getByText } = render(<button onClick={spy}>Get Last Commits</button>);
 
-  // manually trigger the callback
-  //tree.children[1].props.onClick();
-  //tree.props.onClick();
-  // re-rendering
-  // tree = RepoCommit.toJSON();
-  // expect(tree).toMatchSnapshot();
+  fireEvent(
+    getByText("Get Last Commits"),
+    new MouseEvent("click", {
+      bubbles: true, // click events must bubble for React to see it
+      cancelable: true
+    })
+  );
+
+  expect(spy).toHaveBeenCalledTimes(1);
+
+  // // Act
+  // fireEvent.click(getByText("Load Greeting"));
+
+  // // Let's wait until our mocked `get` request promise resolves and
+  // // the component calls setState and re-renders.
+  // // getByTestId throws an error if it cannot find an element with the given ID
+  // // and waitForElement will wait until the callback doesn't throw an error
+  // const greetingTextNode = await waitForElement(() =>
+  //   getByTestId("greeting-text")
+  // );
+
+  // // Assert
+  // expect(axiosMock.get).toHaveBeenCalledTimes(1);
+  // expect(axiosMock.get).toHaveBeenCalledWith(url);
+  // expect(getByTestId("greeting-text")).toHaveTextContent("hello there");
+  // expect(getByTestId("ok-button")).toHaveAttribute("disabled");
+  // // snapshots work great with regular DOM nodes!
+  // expect(container.firstChild).toMatchSnapshot();
 });
-
-// test("Commit renders properly ", () => {
-//   const index = 1;
-//   const message = "Change language to English";
-//   const commit = renderer.create(<Commit index={index} message={message} />);
-//   let tree = commit.toJSON();
-//   expect(tree).toMatchSnapshot();
-// });
