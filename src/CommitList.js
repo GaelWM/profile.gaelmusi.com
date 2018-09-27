42e7dfb235d7b405a4d816401b307322ececa752
Gael Musikingala <gaelmusi0@gmail.com>
2018-09-27 02:49:57 +0200

    Add RepoCommits test using react-testing-library

--- src/CommitList.js
+++ src/CommitList.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 
 const CommitList = props => {
   return <ol className="list-group">{props.commits}</ol>;
--- src/RepoCommit.test.js
+++ src/RepoCommit.test.js
@@ -9,38 +9,48 @@ import {
 // this adds custom jest matchers from jest-dom
 //mport "jest-dom/extend-expect";
 import axiosMock from "axios"; // the mock lives in a __mocks__ directory
+import { get } from "axios";
 import RepoCommits from "./RepoCommits"; // see the tests for a full implementation
+import renderer from "react-test-renderer";
 
 // automatically unmount and cleanup DOM after the test is finished.
 afterEach(cleanup);
 
 test("Fetch makes an API call and displays the commits when Get Last Commits is clicked", async () => {
   //ini
-  const commits = {
-    "0": { index: "1", message: "Change color of a main div" },
-    "1": { index: "1", message: "Change file transfer process to sftp" }
-  };
-  // Arrange
-  //axiosMock.get.mockResolvedValueOnce({ commits });
-  //const url = "https://api.github.com/repos/gaelwm/profile.gaelmusi.com/commits";
 
-  const spy = jest.fn();
-  //const url =
-  //"https://api.github.com/repos/gaelwm/profile.gaelmusi.com/commits";
-  //const { getByTestId, container } = render(<RepoCommits commits={commits} />);
+  const commits = [
+    {
+      commit: {
+        message: "Change the website backgroung image"
+      }
+    },
+    {
+      commit: {
+        message: "Add Login to website"
+      }
+    }
+  ];
 
-  const { getByText, getByTestId } = render(<RepoCommits />);
-
-  fireEvent(
-    getByText("Get Last Commits"),
-    new MouseEvent("click", {
-      bubbles: true, // click events must bubble for React to see it
-      cancelable: true
+  axiosMock.get.mockImplementation(() =>
+    Promise.resolve({
+      data: commits
     })
   );
 
-  //await waitForElement(() => getByTestId("testRepoCommit"));
+  const component = render(<RepoCommits />);
+  const btnGetCommits = component.getByText("Get Last Commits");
+  btnGetCommits.click();
+  const repocommits = await component;
 
-  expect(spy).toHaveBeenCalledTimes(1);
-  // expect(axiosMock.get).toHaveBeenCalledWith(url);
+  const textBtnGetCommits = btnGetCommits.textContent;
+
+  expect(textBtnGetCommits).toBe("Get Last Commits");
+  expect(axiosMock.get).toHaveBeenCalledTimes(1);
+  expect(axiosMock.get).toHaveBeenLastCalledWith(
+    "https://api.github.com/repos/gaelwm/profile.gaelmusi.com/commits"
+  );
+
+  expect(repocommits).toEqual(component);
+  expect(component).toMatchSnapshot();
 });
